728cb78702de32d2fa72826f7834f7c091b17cea
André Fontes <andrefontes@gmail.com>
2024-06-04 14:18:03 -0300

    Alteração

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ import {
   Tbody,
   Td,
   useBreakpointValue,
+  Grid, 
+  GridItem,
+  Container,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import ModalComp from "./components/ModalComp";
@@ -49,59 +52,62 @@ const App = () => {
       fontSize="20px"
       fontFamily="poppins"
     >
-      <Box maxW={1300} w="100%" h="100vh" py={10} px={2}>
-        <Button colorScheme="blue" onClick={() => [setDataEdit({}), onOpen()]}>
-          + Adicionar Funcionário
-        </Button>
+    
 
-        <Box overflowY="auto" height="100%">
-          <Table mt="6">
-            <Thead>
-              <Tr>
-                <Th maxW={isMobile ? 5 : 100} fontSize="20px">
-                  Nome
-                </Th>
-                <Th maxW={isMobile ? 5 : 100} fontSize="20px">
-                  E-Mail
-                </Th>
-                <Th maxW={isMobile ? 5 : 100} fontSize="20px">
-                  Cpf
-                </Th>
-                <Th maxW={isMobile ? 5 : 100} fontSize="20px">
-                  Ativo
-                </Th>
-                <Th p={0}></Th>
-                <Th p={0}></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {data.map(({ name, email, cpf, rg, nascimento, sexo, ativo, cargo }, index) => (
-                <Tr key={index} cursor="pointer " _hover={{ bg: "gray.100" }}>
-                  <Td maxW={isMobile ? 5 : 100}>{name}</Td>
-                  <Td maxW={isMobile ? 5 : 100}>{email}</Td>
-                  <Td maxW={isMobile ? 5 : 100}>{cpf}</Td>
-                  <Td maxW={isMobile ? 5 : 100}>{ativo}</Td>
-                  <Td p={0}>
-                    <EditIcon
-                      fontSize={20}
-                      onClick={() => [
-                        setDataEdit({ name, email, cpf, rg, nascimento, sexo, ativo, cargo, index }),
-                        onOpen(),
-                      ]}
-                    />
-                  </Td>
-                  <Td p={0}>
-                    <DeleteIcon
-                      fontSize={20}
-                      onClick={() => handleRemove(email)}
-                    />
-                  </Td>
-                </Tr>
-              ))}
-            </Tbody>
-          </Table>
-        </Box>
-      </Box>
+      
+          <Box maxW={1300} w="100%" h="100vh" py={10} px={2}>
+                    <Button colorScheme="white" color="#649fbf" border="solid 1px #649fbf" width="100%" onClick={() => [setDataEdit({}), onOpen()]}>
+                      + Adicionar Funcionário
+                    </Button>
+
+                    <Box overflowY="auto" height="100%">
+                      <Table mt="6">
+                        <Thead>
+                          <Tr>
+                            <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                              Nome
+                            </Th>
+                            <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                              E-Mail
+                            </Th>
+                            <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                              Cpf
+                            </Th>
+                            <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                              Ativo
+                            </Th>
+                            <Th p={0}></Th>
+                            <Th p={0}></Th>
+                          </Tr>
+                        </Thead>
+                        <Tbody>
+                          {data.map(({ name, email, cpf, rg, nascimento, sexo, ativo, cargo }, index) => (
+                            <Tr key={index} cursor="pointer " _hover={{ bg: "gray.100" }}>
+                              <Td maxW={isMobile ? 5 : 100}>{name}</Td>
+                              <Td maxW={isMobile ? 5 : 100}>{email}</Td>
+                              <Td maxW={isMobile ? 5 : 100}>{cpf}</Td>
+                              <Td maxW={isMobile ? 5 : 100}>{ativo}</Td>
+                              <Td p={0}>
+                                <EditIcon
+                                  fontSize={20}
+                                  onClick={() => [
+                                    setDataEdit({ name, email, cpf, rg, nascimento, sexo, ativo, cargo, index }),
+                                    onOpen(),
+                                  ]}
+                                />
+                              </Td>
+                              <Td p={0}>
+                                <DeleteIcon
+                                  fontSize={20}
+                                  onClick={() => handleRemove(email)}
+                                />
+                              </Td>
+                            </Tr>
+                          ))}
+                        </Tbody>
+                      </Table>
+                    </Box>
+                  </Box>
       {isOpen && (
         <ModalComp
           isOpen={isOpen}
--- src/components/ModalComp.jsx
+++ src/components/ModalComp.jsx
@@ -1,11 +1,11 @@
 //import { useForm, UseFormRegisterReturn } from 'react-hook-form'
-//import { FiFile } from 'react-icons/fi'
+import { FiFile } from 'react-icons/fi'
 import {
     FileUpload
     ,FileUploadDropzone
   } from '@saas-ui/file-upload'
 
-  import { FiFile } from 'react-icons/fi'
+  import '/styles.css';
 
   //FileUploadButton
 
@@ -30,9 +30,12 @@ import {
     Stack,
     HStack,
     RadioGroup,
+    Container,
     Text,
     Select,
     Box,
+    Grid, 
+    GridItem
   } from "@chakra-ui/react";
   import { useState, ReactNode, useRef } from "react";
   
@@ -80,87 +83,160 @@ import {
       <>
         <Modal isOpen={isOpen} onClose={onClose}>
           <ModalOverlay />
-          <ModalContent>
+          <ModalContent minWidth='700px' width='100%'>
             <ModalHeader>Cadastro de Clientes</ModalHeader>
             <ModalCloseButton />
-            <ModalBody>
+            <ModalBody >
               <FormControl display="flex" flexDir="column" gap={4}>
+              <Container maxW='2xl' bg='#fff' p='4' border='solid 1px #649fbf' borderRadius={10}>
+                    <Grid templateColumns='repeat(2, 1fr)'>
+                        <Box>
+                                <FormLabel>trabalhador está Ativo ou Inativo?</FormLabel>
+                        </Box>
+                        <Box>
+                                <Checkbox 
+                                    value="Ativo"
+                                    onChange={(e) => setAtivo(e.target.value)}
+                                > Ativo </Checkbox>
+                        </Box>
+                    </Grid>
+                </Container>
+              <Container maxW='2xl' bg='#fff' p='4' border='solid 1px #649fbf' borderRadius={10}>
+              <Grid templateColumns='repeat(2, 1fr)'>
+                        <Box mr='4'>
+                        <FormLabel>Nome</FormLabel>
+                        <Input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            w='100%'
+                        />
+                        </Box>
+                        <Box>
+                                <FormLabel>Sexo</FormLabel>
+                                <RadioGroup defaultValue='Feminino' value={sexo}>
+                                <Stack spacing={5} direction='row'>
+                                    <Radio colorScheme='pink' value='Feminino' onChange={(e) => setSexo(e.target.value)}>
+                                        Feminino
+                                    </Radio>
+                                    <Radio colorScheme='blue' value='Masculino' onChange={(e) => setSexo(e.target.value)}>
+                                        Masculino
+                                    </Radio>
+                                </Stack>
+                                </RadioGroup>
+                        </Box>
+                </Grid>
+                <Grid templateColumns='repeat(2, 1fr)'>
+                        <Box mr='4'>
+                                <FormLabel>CPF</FormLabel>
+                                <Input
+                                    type="text"
+                                    value={cpf}
+                                    onChange={(e) => setCpf(e.target.value)}
+                                />
+                        </Box>
+                        <Box>
+                                <FormLabel>Nascimento</FormLabel>
+                                <Input
+                                    type="text"
+                                    value={nascimento}
+                                    onChange={(e) => setNascimento(e.target.value)}
+                                />
+                        </Box>
+                </Grid>
+                <Grid templateColumns='repeat(2, 1fr)'>
+                        <Box mr='4'>
+                                <FormLabel>RG</FormLabel>
+                                <Input
+                                    type="text"
+                                    value={rg}
+                                    onChange={(e) => setRg(e.target.value)}
+                                />
+                        </Box>
+                        <Box>
+                                <FormLabel>Cargo</FormLabel>
+                                <Select onChange={(e) => setCargo(e.target.value)}>
+                                    <option value=''></option>
+                                    <option value='Cargo 1'>Cargo 1</option>
+                                    <option value='Cargo 2'>Cargo 2</option>
+                                    <option value='Cargo 3'>Cargo 3</option>
+                                </Select>
+                        </Box>
+                </Grid>
+
+                </Container>
+            <Container maxW='2xl' bg='#fff' p='4' border='solid 1px #649fbf' borderRadius={10}>
+                        <Box>
+                                <FormLabel>Quais EPIs o trabalhador usa na atividade?</FormLabel>
+                        </Box>
+                        <Box mb='6'>
+                                <Checkbox 
+                                    value="Ativo"
+                                    onChange={(e) => setAtivo(e.target.value)}
+                                > O trabalhador não usa EPI. </Checkbox>
+                        </Box>
+
+
+                <Container maxW='2xl' bg='#fff' p='4' border='solid 1px #649fbf' borderRadius={10}>
+
+                            <Grid templateColumns='repeat(1, 1fr)'>
+                                    <Box>
+                                            <FormLabel>Selecione a atividade</FormLabel>
+                                            <Select onChange={(e) => setCargo(e.target.value)}>
+                                                <option value=''></option>
+                                                <option value='Cargo 1'>Cargo 1</option>
+                                                <option value='Cargo 2'>Cargo 2</option>
+                                                <option value='Cargo 3'>Cargo 3</option>
+                                            </Select>
+                                    </Box>
+                            </Grid>
+                            <Grid templateColumns='repeat(3, 1fr)'>
+                                    <Box mr='4'>
+                                            <FormLabel>Selecione o EPI</FormLabel>
+                                            <Select onChange={(e) => setCargo(e.target.value)}>
+                                                <option value=''></option>
+                                                <option value='Cargo 1'>Cargo 1</option>
+                                                <option value='Cargo 2'>Cargo 2</option>
+                                                <option value='Cargo 3'>Cargo 3</option>
+                                            </Select>
+                                    </Box>
+                                    <Box mr='4'>
+                                            <FormLabel>Informe o número CA</FormLabel>
+                                            <Input
+                                                type="text"
+                                                value={rg}
+                                                onChange={(e) => setRg(e.target.value)}
+                                            />
+                                    </Box>
+                                    <Box>
+                                            <FormLabel>&nbsp;</FormLabel>
+                                            <Button colorScheme="white" color="#649fbf" border="solid 1px #649fbf" width="100%">Adicionar EPI</Button>
+                                    </Box>
+                            </Grid>
+                </Container>
+                <Button colorScheme="white" color="#649fbf" border="solid 1px #649fbf" width="100%" mt='5' >Adicionar outra atividade</Button>
+
+
+                </Container>
+                <Container maxW='2xl' bg='#fff' p='4' border='solid 1px #649fbf' borderRadius={10}>
                 <Box>
-                  <FormLabel>Nome</FormLabel>
-                  <Input
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                </Box>
-                <Box>
-                  <FormLabel>E-mail</FormLabel>
-                  <Input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </Box>
-                <Box>
-                  <FormLabel>CPF</FormLabel>
-                  <Input
-                    type="text"
-                    value={cpf}
-                    onChange={(e) => setCpf(e.target.value)}
-                  />
-                </Box>
-                <Box>
-                  <FormLabel>RG</FormLabel>
-                  <Input
-                    type="text"
-                    value={rg}
-                    onChange={(e) => setRg(e.target.value)}
-                  />
-                </Box>
-                <Box>
-                  <FormLabel>Nascimento</FormLabel>
-                  <Input
-                    type="text"
-                    value={nascimento}
-                    onChange={(e) => setNascimento(e.target.value)}
-                  />
-                </Box>
-                <Box>
-                  <FormLabel></FormLabel>
-                  <Checkbox 
-                    value="Ativo"
-                    onChange={(e) => setAtivo(e.target.value)}
-                  > Ativo </Checkbox>
-                </Box>
-                <Box>
-                  <FormLabel>Sexo</FormLabel>
-                  <RadioGroup defaultValue='Feminino' value={sexo}>
-                  <Stack spacing={5} direction='row'>
-                    <Radio colorScheme='pink' value='Feminino' onChange={(e) => setSexo(e.target.value)}>
-                        Feminino
-                    </Radio>
-                    <Radio colorScheme='blue' value='Masculino' onChange={(e) => setSexo(e.target.value)}>
-                        Masculino
-                    </Radio>
-                  </Stack>
-                  </RadioGroup>
-                </Box>
-                <Box>
-                <FormLabel>Cargo</FormLabel>
-                <Select onChange={(e) => setCargo(e.target.value)}>
-                    <option value=''></option>
-                    <option value='Cargo 1'>Cargo 1</option>
-                    <option value='Cargo 2'>Cargo 2</option>
-                    <option value='Cargo 3'>Cargo 3</option>
-                </Select>
-                </Box>
-                <Box>
+                    <FormLabel>Adicione Atestado de Saúde (opcional):</FormLabel>
+                    <Input
+                                                    type="text"
+                                                    value={rg}
+                                                    onChange={(e) => setRg(e.target.value)}
+                                                    w='100%'
+                                                />
+
+                    
                 <FileUpload maxFileSize={1024 * 1024} maxFiles={1} accept="image/*">
                     {({ files, deleteFile }) => (
                         <FileUploadDropzone>
                         <Text fontSize="sm">Arraste imagem aqui</Text>
                         {!files?.length ? (
-                            <Button as={Button} leftIcon={<Icon as={FiFile} />}>Selecionar arquivo</Button>
+                            //<input type="file" />
+                            <Button colorScheme="white" color="#649fbf" border="solid 1px #649fbf" width="100%" mt='5' >Selecionar arquivo</Button>
+                            //<Button type="file" as={Button} leftIcon={<Icon as={FiFile} />}>Selecionar arquivo</Button>
                         ) : (
                             <HStack>
                             <Text fontSize="sm">{files[0].name}</Text>
@@ -170,7 +246,7 @@ import {
                                 deleteFile(files[0])
                                 }}
                             >
-                                Clear
+                                Remover
                             </Button>
                             </HStack>
                         )}
@@ -178,8 +254,16 @@ import {
                     )}
                     </FileUpload>
                 </Box>
+                </Container>
 
-
+                <Box>
+                  <FormLabel>E-mail</FormLabel>
+                  <Input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Box>
 
               </FormControl>
             </ModalBody>
